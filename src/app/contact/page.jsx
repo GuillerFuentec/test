40c0673321dd584048e62c8d4e77cd0c69520ba9
GuillerFuentec <copelloguillermo60@gmail.com>
2024-06-29 18:16:00 -0400

    first commit

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div className="bg-black h-screen text-cyan-500">
       <h1>Contact Me</h1>
-      <Link href="https://ghostscorp.net/">home</Link>
+      <Link href="/">home</Link>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-black h-screen text-cyan-500">
     <h1>Hello World!</h1>
-    <Link href="https://ghostscorp.net/contact">contact</Link>
+    <Link href="/contact">contact</Link>
   </div>
   );
 }
